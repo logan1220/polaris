20b36293e644821471ed57c3e4391ca88b1d28fa
Logan <logo1220@gmail.com>
2018-06-16 01:14:30 -0400

    Create home page, icon, and some styles

--- App.js
+++ App.js
@@ -1,26 +1,55 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    createBottomTabNavigator,
+    createStackNavigator
+} from 'react-navigation';
+import { YellowBox } from 'react-native';
+import Home from './src/containers/Home';
+import Test from './src/containers/Test';
+import { Feather } from '@expo/vector-icons';
+
+YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
+
+const Tabs = createBottomTabNavigator({
+    Home: {
+        screen: Home,
+        icon: (<Feather name="home" size={32} color="white" />)
+    },
+    Test: {
+        screen: Test,
+    }
+}, {
+    navigationOptions: {
+        tabBarIcon: (<Feather name="home" size={32} color="white" />)
+    },
+    tabBarOptions: {
+        style: {
+            backgroundColor: '#989998',
+        },
+        showIcon: true,
+        activeTintColor: 'white',
+        inactiveTintColor: 'gray',
+    }
+});
+
+const RootStack = createStackNavigator({
+    Tab: {
+        screen: Tabs,
+    },
+}, {
+    navigationOptions: {
+        headerTitle: 'test',
+        headerStyle: {
+            backgroundColor: '#79ed81'
+        }
+    },
+});
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-          <View>
 
-          </View>
-      </View>
-    );
-  }
+    render() {
+        return (
+            <RootStack />
+        );
+    }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
